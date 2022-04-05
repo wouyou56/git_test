c150094a02c9c33ed8085e8c0b16d57f6e7b5ca5
wouyou56 <82192353+wouyou56@users.noreply.github.com>
2022-04-05 16:54:54 +0800

    pull test

--- src/main/java/com/xwy/git/GitTest.java
+++ src/main/java/com/xwy/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git555555!");
         System.out.println("hello git666666!");
         System.out.println("push test!");
+         System.out.println("pull test!");
     }
 }
